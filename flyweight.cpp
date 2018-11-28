a386e17df0f4f327bba5831eff87213f112f173b
Kevin Salazar <kast46.15@gmail.com>
2018-11-28 16:45:17 -0500

    Update flyweight.cpp

--- flyweight.cpp
+++ flyweight.cpp
@@ -41,7 +41,7 @@ public:
 		auto it = map_radioColor.find(make_pair(radio,color));
 
 		if(it != map_radioColor.end()){// si ya hay fábrica de esas pelotas
-			return Pelota<Posicion,Radio,Color> (posicion,(*it).second);	
+			return Pelota<Posicion,Radio,Color> (posicion,it->second);	
 		}
 
 		Fabrica_pelotas<Radio,Color> *fabrica_pelotas = new Fabrica_pelotas<Radio,Color>(radio, color);
